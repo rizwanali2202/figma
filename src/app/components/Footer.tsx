408e2e2addb29431e4b8e174ced0e6e30a29ba6f
Rizwan Ali <Rizwanali2202@gmail.com>
2024-11-01 21:39:05 +0500

    Commit
    Latest Project and assigment

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,22 @@
+
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <>
+      <div className="Footer">
+        <div className="iconsDiv">
+        <i className="bi bi-facebook"></i>
+        <i className="bi bi-linkedin"></i>
+        <i className="bi bi-instagram"></i>
+        <i className="bi bi-twitter"></i>
+
+
+
+            </div>
+
+           <p className="copyright">Copyright ©2024 All rights reserved</p> 
+      </div>
+      </>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,128 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import Card from "@/app/components/recent-post-card";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="hero-section">
+        <div className="continer">
+          <div className="main">
+            <div className="text-continer">
+              <h1 className="main-text">Hi, i am John,
+                creative Technologist</h1>
+              <p className="main-para">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.</p>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+              <button className="resume-download">Download Resume</button>
+            </div>
+            <div>
+              <img src="./image.png" alt="image" width={300} height={300} />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+
+
+      <div className="recent-post">
+        <div className="continer">
+
+          <div className="main">
+
+            <div className="heading">
+              <h3>Recent Post</h3>
+              <button className="view-all-button"> View All</button>
+            </div>
+
+            <div className="card-parent">
+
+              <Card />
+              <Card />
+
+            </div>
+
+          </div>
+
+
+
+
+
+
+        </div>
+      </div>
+
+      <div className="feature-section">
+        <div className="continer">
+          <div className="main">
+
+            <div className="main-heading">
+              <h3>Featured Works</h3>
+            </div>
+
+                <div className="card-parents"></div>
+
+                  <div className="card">
+              <Image src="/image 1.png" width={246} height={180} alt="Picture"></Image>
+
+              <div className="card-content">
+                <h2>Designing Dashboards</h2>
+                <div className="badge-parent">
+                  <div className="badge">
+                    <p>2020</p>
+                  </div>
+                    <p>Dashboard</p>
+                </div>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+              </div>
+                  </div>
+
+                  <div className="card">
+              <Image src="/image2.png" width={246} height={180} alt="Picture"></Image>
+
+              <div className="card-content">
+                <h2>Designing Dashboards</h2>
+                <div className="badge-parent">
+                  <div className="badge">
+                    <p>2020</p>
+                  </div>
+                    <p>Dashboard</p>
+                </div>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+              </div>
+                </div>
+
+                <div className="card">
+              <Image src="/image3.png" width={246} height={180} alt="Picture"></Image>
+
+              <div className="card-content">
+                <h2>Designing Dashboards</h2>
+                <div className="badge-parent">
+                  <div className="badge">
+                    <p>2020</p>
+                  </div>
+                    <p>Dashboard</p>
+                </div>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+              </div> 
+            
+              </div>
+
+              </div>
+
+        </div>
+      </div>
+
+
+    </>
+
+
   );
 }
